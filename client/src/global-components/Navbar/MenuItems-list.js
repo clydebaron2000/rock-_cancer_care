c4977bd3997b73627396260d00e100d13ae6e4e2
Clyde Baron Rapinan <clydebaron2000@gmail.com>
2021-05-19 12:05:50 -0700

    added scss filesystem and min.css generation.

---
+++ client/src/global-components/Navbar/MenuItems-list.js
@@ -0,0 +1,23 @@
+// ensure this is similar to file structure
+export const MenuItems = [
+    {
+        title: 'HOME',
+        url: '/',
+        style: 'underline'
+    },    
+    {
+        title: 'ABOUT US',
+        url: '/about',
+        style: 'underline'
+    },
+    {
+        title: 'CONTACT',
+        url: '/contact',
+        style: 'underline'
+    },    
+    {
+        title: 'INVEST WITH US',
+        url: '/invest',
+        style: 'underline'
+    }
+]
