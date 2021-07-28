11a03aa7664aa4c69e243be1c91c974e76bd6e8b
Clyde Baron Rapinan <clydebaron2000@gmail.com>
2021-07-28 14:59:39 -0700

    deleted sh file

--- client/copy_to_deployable.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# TODO complete
-PARENT_PATH="$(dirname $(realpath $0))"
-
-function main {
-    # cp -r * ../../cyruscapitalfund/client/
-    tar cf - --exclude=node_modules . | (cd ../../cyruscapitalfund/client && tar xvf - )
-}
-main "$@"
