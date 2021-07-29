f6a0e90d83a920104f60e2673f4d98d8719eef32
Clyde Baron Rapinan <clydebaron2000@gmail.com>
2021-07-29 05:42:37 -0700

    modified route order

--- client/src/App.js
+++ client/src/App.js
@@ -16,14 +16,14 @@ function App() {
   return (
     <Router>
       <Switch>
-          {/* <Route path="/" exact component={Admin} /> */}
         <UserProvider>
+          {/* <Route path="/" exact component={Admin} /> */}
           {/* <Route path="/" exact component={Admin} /> */}
           <Route path="/volunteer-form" exact component={VolunteerForm} />
           <Route path="/patient-form" exact component={PatientForm} />
           <Route path="/admin" exact component={Admin} />
-        </UserProvider>
           <Route component={NotFound}/> 
+        </UserProvider>
       </Switch>
     </Router>
   )
