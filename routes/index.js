ef74113cc9b271eb16d3240fb9def080c02d9c61
Clyde Baron Rapinan <clydebaron2000@gmail.com>
2021-08-19 16:54:43 -0700

    maybe

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,6 @@ router.use("/api", apiRoutes)
 
 // If no API routes are hit, send the React app
 router.use((req, res)=> {
-  if (process.env.NODE_ENV==="production")
-    res.sendFile(path.join(__dirname, "../client/build/index.html"))
-    else
     res.sendFile(path.join(__dirname, "../client/public/index.html"))
 });
 
